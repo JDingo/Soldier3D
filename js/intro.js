fd2b06d164b3f534d5696f57ce2aeae96c2d12a0
JDingo <45366094+JDingo@users.noreply.github.com>
2018-11-26 19:38:25 +0200

    First commit

---
+++ js/intro.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('intro_button');
+var intro_screen = document.getElementById('intro')
+
+button.onclick = function() {
+    intro_screen.classList.add('transition')
+}
---
+++ js/viewer.js
@@ -0,0 +1,98 @@
+var canvas = document.getElementById('viewer');
+
+var clock = new THREE.Clock();
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color().setHSL(0.6, 0, 1);
+scene.fog = new THREE.Fog(scene.background, 1, 5000);
+
+var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(2, 2, 1);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth/2, window.innerHeight/2);
+renderer.gammaOutput = true;
+renderer.gammaFactor = 3.2;
+renderer.shadowMap.enabled = true;
+canvas.appendChild(renderer.domElement);
+
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 1, 0);
+controls.update();
+
+// Ground
+var groundGeo = new THREE.PlaneBufferGeometry(10000, 10000);
+var groundMat = new THREE.MeshPhongMaterial({ color: 0xffffff, specular: 0x050505 });
+groundMat.color.setHSL(0.095, 1, 0.75);
+var ground = new THREE.Mesh(groundGeo, groundMat);
+ground.rotation.x = - Math.PI / 2;
+ground.position.y = - 0.02;
+ground.receiveShadow = true;
+scene.add(ground);
+
+
+// Sky
+var skyGeo = new THREE.SphereBufferGeometry(100, 100, 100);
+var skyMat = new THREE.MeshPhongMaterial( { color: 0x66ccff } );
+skyMat.side = THREE.DoubleSide;
+var sky = new THREE.Mesh( skyGeo, skyMat );
+scene.add(sky);
+
+// Lightning 
+var dirLightFront = new THREE.DirectionalLight(0xFFFFFF);
+dirLightFront.color.setHSL(0.1, 1, 0.95);
+dirLightFront.position.set(-1, 1.75, 1);
+dirLightFront.position.multiplyScalar(30);
+dirLightFront.castShadow = true;
+scene.add(dirLightFront);
+
+dirLightHelperFront = new THREE.DirectionalLightHelper(dirLightFront, 10);
+scene.add(dirLightHelperFront);
+
+var dirLightBack = new THREE.DirectionalLight(0xFFFFFF);
+dirLightBack.color.setHSL(0.1, 1, 0.95);
+dirLightBack.position.set(1, -1.75, -1);
+dirLightBack.position.multiplyScalar(30);
+dirLightBack.castShadow = true;
+scene.add(dirLightBack);
+
+dirLightHelperBack = new THREE.DirectionalLightHelper(dirLightBack, 10);
+scene.add(dirLightHelperBack);
+
+var hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+hemiLight.position.set(0, 10, 0);
+scene.add(hemiLight);
+
+var ambientLight = new THREE.AmbientLight(0x404040);
+scene.add(ambientLight);
+
+// Model Loader
+function loadModel() {
+    var loader = new THREE.GLTFLoader();
+    loader.load('http://localhost:8000/Model.gltf', function (gltf) {
+
+        model = gltf.scene;
+        model.animations = gltf.animations;
+        scene.add(model);
+
+        mixer = new THREE.AnimationMixer(model);
+        mixer.clipAction('Action').play()
+        console.log(mixer);
+        animate();
+    },
+
+        undefined, function (e) {
+            console.error(e);
+        }
+    );
+}
+
+function animate() {
+    timeDelta = clock.getDelta();
+    mixer.update(timeDelta);
+
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+};
+
+loadModel();
